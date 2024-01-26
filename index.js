fcee9ba82e549e44a5248b78ff691f3dd21860c4
Ashish Kumar Singh <djdaydreamer.singh4@gmail.com>
2024-01-26 16:25:49 +0530

    first code non flowing read

--- index.js
+++ index.js
@@ -1,9 +1,19 @@
 console.log("hello streams");
-// Code for non-flowing readable mode.
-process.stdin.on("readable", () => {
-  let chunk;
-  console.log("New data available!");
-  while ((chunk = process.stdin.read()) !== null) {
-    console.log(`Chunk read (${chunk.length} bytes): "${chunk.toString()}"`);
-  }
-});
+// Code for non-flowing readable mode. Data is pulled using read() method
+// process.stdin.on("readable", () => {
+//   let chunk;
+//   console.log("New data available!");
+//   while ((chunk = process.stdin.read()) !== null) {
+//     console.log(`Chunk read (${chunk.length} bytes): "${chunk.toString()}"`);
+//   }
+// });
+
+// Code for flowing mode. Data is pulled on "data event".
+// process.stdin
+//   .on("data", (chunk) => {
+//     console.log("New data available!");
+//     console.log(`Chunk read (${chunk.length} bytes): "${chunk.toString()}"`);
+//   })
+//   .on("end", () => {
+//     console.log("End of Stream.");
+//   });
