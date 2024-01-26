53adef58e05c3bc4633f8a4d7ac6628a7b5c6cce
Ashish Kumar Singh <djdaydreamer.singh4@gmail.com>
2024-01-26 21:10:06 +0530

    streams comp, remain

--- csv_op.js
+++ csv_op.js
@@ -3,7 +3,9 @@ import path from "path";
 import { parse } from "csv-parse";
 import { FilterByPopularity } from "./filterByPopularity.js";
 import { TotalDocsProcessed } from "./totalDocsProcessed.js";
-import { PassThrough } from "stream";
+import { PassThrough, pipeline } from "stream";
+import Pumpify from "pumpify";
+import MultiStream from "multistream";
 
 // import { Transform } from "stream";
 
@@ -26,10 +28,25 @@ moniter.write("Ended!");
 moniter.end();
 // console.log(path.resolve("./datasets/genres.csv"));
 
-createReadStream(path.resolve("./datasets/shows.csv"))
-  .pipe(csvParser)
-  .pipe(new FilterByPopularity(200))
-  .pipe(new TotalDocsProcessed())
-  //   .pipe(createWriteStream("output.txt"));
-  .pipe(moniter)
-  .pipe(process.stdout);
+// createReadStream(path.resolve("./datasets/shows.csv"))
+//   .pipe(csvParser)
+//   .pipe(new FilterByPopularity(200))
+//   .pipe(new TotalDocsProcessed())
+//   //   .pipe(createWriteStream("output.txt"));
+//   .pipe(moniter)
+//   .pipe(process.stdout);
+
+pipeline(
+  createReadStream(path.resolve("./datasets/shows.csv")),
+  csvParser,
+  new FilterByPopularity(200),
+  new TotalDocsProcessed(),
+  //   moniter,
+  process.stdout,
+  (err) => {
+    if (err) {
+      console.log(err);
+      process.exit(1);
+    }
+  }
+);
